892b8a60a924dff581ae3259274e7ba44f361836
Amanuel Galema <amexabee@yahoo.com>
2023-12-15 15:34:35 +0200

    Fix linters

--- src/redux/swimClass/swimClass.js
+++ src/redux/swimClass/swimClass.js
@@ -15,7 +15,7 @@ export const getSwimClasses = createAsyncThunk(
     });
     const swimClasses = await response.json();
     return swimClasses;
-  }
+  },
 );
 
 export const deleteClass = createAsyncThunk(
@@ -33,7 +33,7 @@ export const deleteClass = createAsyncThunk(
     } catch (e) {
       return e.error;
     }
-  }
+  },
 );
 
 export const postClass = createAsyncThunk(
@@ -52,7 +52,7 @@ export const postClass = createAsyncThunk(
       }
       throw new Error('Something went wrong');
     });
-  }
+  },
 );
 export const updateClass = createAsyncThunk(
   'swimClasses/updateClass',
@@ -70,7 +70,7 @@ export const updateClass = createAsyncThunk(
       }
       throw new Error('Something went wrong');
     });
-  }
+  },
 );
 
 export const swimClassesSlice = createSlice({
@@ -99,7 +99,7 @@ export const swimClassesSlice = createSlice({
     builder.addCase(deleteClass.fulfilled, (state, action) => {
       state.status = 'success';
       state.swimClasses = state.swimClasses.filter(
-        (swimClass) => swimClass.id !== action.payload
+        (swimClass) => swimClass.id !== action.payload,
       );
     });
     builder.addCase(deleteClass.rejected, (state) => {
